fb2d930095e26b8a36559bb989c15839a200f53f
TamrielNetwork <contact@tamriel.me>
2022-01-13 16:01:58 +0100

    Updated License Headears for GPL-3.0

--- src/main/java/com/tamrielnetwork/specplayer/commands/SpecPlayerCmd.java
+++ src/main/java/com/tamrielnetwork/specplayer/commands/SpecPlayerCmd.java
@@ -1,6 +1,18 @@
 /*
+ SpecPlayer is a Spigot Plugin, to give players the ability to spectate others.
  Copyright (C) 2022  Leopold Meinel
- Visit https://github.com/TamrielNetwork/SpecPlayer/blob/main/LICENSE for more details.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see https://github.com/TamrielNetwork/SpecPlayer/blob/main/LICENSE.
  */
 package com.tamrielnetwork.specplayer.commands;
 
