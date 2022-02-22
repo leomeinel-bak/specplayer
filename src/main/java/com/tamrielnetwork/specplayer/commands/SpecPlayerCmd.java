134e64b89c95f3c7738fc619aac9ed58911992a3
TamrielNetwork <contact@tamriel.me>
2022-02-22 02:15:50 +0100

    Improve readability

--- src/main/java/com/tamrielnetwork/specplayer/commands/SpecPlayerCmd.java
+++ src/main/java/com/tamrielnetwork/specplayer/commands/SpecPlayerCmd.java
@@ -18,7 +18,8 @@
 
 package com.tamrielnetwork.specplayer.commands;
 
-import com.tamrielnetwork.specplayer.utils.Utils;
+import com.tamrielnetwork.specplayer.utils.commands.Cmd;
+import com.tamrielnetwork.specplayer.utils.commands.CmdSpec;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
@@ -29,79 +30,58 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
+import java.util.UUID;
 
 public class SpecPlayerCmd implements CommandExecutor {
 
-	final HashMap<Player, Location> lastLocation = new HashMap<>();
+	public static final HashMap<UUID, Location> lastLocation = new HashMap<>();
 
 	@Override
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-		// Check args length
-		if (args.length == 0) {
-			executeBack(sender);
-		} else {
-			executeSpectate(sender, args);
-		}
 
+		if (Cmd.isArgsLengthGreaterThan(sender, args, 1)) {
+			return true;
+		}
+		if (Cmd.isArgsLengthEqualTo(sender, args, 0)) {
+			doBack(sender);
+			return true;
+		}
+		doSpec(sender, args);
 		return true;
 	}
 
-	private void executeSpectate(CommandSender sender, String[] args) {
-		// Check if command sender is a player
-		if (!(sender instanceof Player)) {
-			Utils.sendMessage(sender, "player-only");
-			return;
-		}
-		// Check args length
-		if (args.length != 1) {
-			Utils.sendMessage(sender, "no-args");
-			return;
-		}
-		// Check permissions
-		if (!sender.hasPermission("specplayer.spectate")) {
-			Utils.sendMessage(sender, "no-perms");
-			return;
-		}
-		// Set player
+	private void doSpec(@NotNull CommandSender sender, @NotNull String[] args) {
+		Player senderPlayer = (Player) sender;
+		UUID senderUUID = senderPlayer.getUniqueId();
 		Player player = Bukkit.getPlayer(args[0]);
-		// Check if the given player is registered
-		if (player == null) {
-			Utils.sendMessage(sender, "invalid-player");
+
+		if (CmdSpec.isInvalidCmd(sender, player, "specplayer.spectate")) {
 			return;
 		}
-		// Check that player is  not already spectating
-		if (((Player) sender).getGameMode() != GameMode.SPECTATOR) {
-			// Set sender gamemode to spectator
-			((Player) sender).setGameMode(GameMode.SPECTATOR);
-			// Set senderLocation
-			@NotNull Location senderLocation = ((Player) sender).getLocation();
-			// Save senderLocation to lastLocation
-			lastLocation.put((Player) sender, senderLocation);
+
+		assert player != null;
+		if (!lastLocation.containsKey(senderUUID)) {
+			lastLocation.put(senderUUID, senderPlayer.getLocation());
 		}
-		// Teleport sender to player
-		((Player) sender).teleport(player.getLocation());
+		senderPlayer.setGameMode(GameMode.SPECTATOR);
+		senderPlayer.teleport(player.getLocation());
+
 	}
 
-	private void executeBack(CommandSender sender) {
-		// Check if command sender is a player
-		if (!(sender instanceof Player)) {
-			Utils.sendMessage(sender, "player-only");
-			return;
-		}
-		// Check permissions
-		if (!sender.hasPermission("specplayer.spectate")) {
-			Utils.sendMessage(sender, "no-perms");
-			return;
-		}
-		// Set gamemode to survival
-		((Player) sender).setGameMode(GameMode.SURVIVAL);
+	private void doBack(@NotNull CommandSender sender) {
+		Player senderPlayer = (Player) sender;
+		UUID senderUUID = senderPlayer.getUniqueId();
 
-		// Teleport sender to lastLocation
-		if (lastLocation.containsKey((Player) sender)) {
-			((Player) sender).teleport(lastLocation.get((Player) sender));
-			((Player) sender).setAllowFlight(true);
-			((Player) sender).setFlying(true);
-			lastLocation.remove((Player) sender);
+		if (CmdSpec.isInvalidCmd(sender, "specplayer.spectate")) {
+			return;
+		}
+
+		senderPlayer.setGameMode(GameMode.SURVIVAL);
+		if (lastLocation.containsKey(senderUUID)) {
+			senderPlayer.teleport(lastLocation.get(senderUUID));
+			senderPlayer.setAllowFlight(true);
+			senderPlayer.setFlying(true);
+			lastLocation.remove(senderUUID);
 		}
 	}
 }
