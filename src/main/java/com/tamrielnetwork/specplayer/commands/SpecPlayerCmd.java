396b00db7c319d949629b2e2dc805530616949cb
TamrielNetwork <contact@tamriel.me>
2022-03-15 02:37:56 +0100

    Replace unnecessary boolean in Cmd.java

--- src/main/java/com/tamrielnetwork/specplayer/commands/SpecPlayerCmd.java
+++ src/main/java/com/tamrielnetwork/specplayer/commands/SpecPlayerCmd.java
@@ -44,7 +44,7 @@ public class SpecPlayerCmd
 		if (Cmd.isArgsLengthGreaterThan(sender, args, 1)) {
 			return false;
 		}
-		if (Cmd.isArgsLengthEqualTo(args, 0)) {
+		if (args.length == 0) {
 			doBack(sender);
 			return true;
 		}
--- src/main/java/com/tamrielnetwork/specplayer/utils/commands/Cmd.java
+++ src/main/java/com/tamrielnetwork/specplayer/utils/commands/Cmd.java
@@ -29,10 +29,6 @@ public class Cmd {
 		throw new IllegalStateException("Utility class");
 	}
 
-	public static boolean isArgsLengthEqualTo(@NotNull String[] args, int length) {
-		return args.length == length;
-	}
-
 	public static boolean isArgsLengthGreaterThan(@NotNull CommandSender sender, @NotNull String[] args, int length) {
 		if (args.length > length) {
 			Chat.sendMessage(sender, "cmd");
